016a5011a32d388b7d213acbcd7b141d781e2786
Kevin <kevin41212@gmail.com>
2025-03-24 18:08:56 -0400

    Added comment headers

--- JS/p2main.js
+++ JS/p2main.js
@@ -1,3 +1,10 @@
+/*
+    Name: Kevin Thomas
+    Filename: p2.html
+    Date: 2025-03-24
+    Description: js code that makes an image libary by looping thorugh images 
+    and it gives the user the option to darken the images
+*/
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -24,8 +31,12 @@ for (const image of images) {
     newImage.addEventListener('click', e => {displayedImage.src = e.target.src; displayedImage.alt = e.target.alt;});
   }  
 
-/* Wiring up the Darken/Lighten button */
-/*
+/* Wiring up the Darken/Lighten button 
+   makes a listener for the btn that listens for a click on the btn
+   creates a constant that gets the class atribute from the btn
+   if that atribute is dark then it sets the class atribute to light and it changes the text of the btn to lighten and darkens the image
+   else it then it sets the class atribute to dark and it changes the text of the btn to darken and restores the image to original state
+   */
 btn.addEventListener('click', () => {
     const btnClass = btn.getAttribute('class');
     if (btnClass === 'dark') {
@@ -37,4 +48,4 @@ btn.addEventListener('click', () => {
       btn.textContent = 'Darken';
       overlay.style.backgroundColor = 'rgba(0,0,0,0)';
     }
-  });*/
+  });
